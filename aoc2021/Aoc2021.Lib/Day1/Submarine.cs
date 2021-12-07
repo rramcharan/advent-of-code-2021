734533d252dcd18c5b40ab74512169366e031138
Radjesh Ramcharan <r.ramcharan@tjip.com>
2021-12-07 01:48:26 +0100

    day 1 - part 1

---
+++ aoc2021/Aoc2021.Lib/Day1/Submarine.cs
@@ -0,0 +1,43 @@
+﻿using Aoc2021.Lib.Helper;
+
+namespace Aoc2021.Day1
+{
+    public class Submarine
+    {
+        public Submarine()
+        {
+            HasDepthRegistered = false;
+        }
+        public bool HasDepthRegistered { get; set; }
+
+        public static Submarine ReadLines(string text)
+        {
+            var result = new Submarine();
+
+            foreach (var line in text.LineByLine(true))
+            {
+                result.RegisterDepth(int.Parse(line));
+            }
+
+            return result;
+        }
+
+        public int Depth { get; private set; }
+
+        public int NumberOfDepthIncreases { get; set; }
+
+        public void RegisterDepth(int depth)
+        {
+            if (HasDepthRegistered)
+            {
+                if (depth > Depth) NumberOfDepthIncreases++;
+            }
+            else
+            {
+                HasDepthRegistered = true;
+            }
+
+            Depth = depth;
+        }
+    }
+}
---
+++ aoc2021/Aoc2021.Lib/Helper/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aoc2021.Lib.Helper
+{
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> LineByLine(this string text, bool allowEmptyLines = false)
+        {
+            using StringReader reader = new StringReader(text);
+            while (true)
+            {
+                string? line = reader.ReadLine();
+                if (line == null) break;
+
+                line = line.Trim();
+                if (!allowEmptyLines)
+                {
+                    if (string.IsNullOrEmpty(line))
+                        throw new Exception("Empty lines are not allowed");
+                }
+
+                yield return line;
+            }
+        }
+    }
+
+}
